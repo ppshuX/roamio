98ac4f8697f07f3c3edf4ec9809fd458c9d58493
wenxiao Lv <2064747320@qq.com>
2025-12-28 17:37:48 +0800

    fix: update Ralendar logo filename from ralendar_logo_final.png to Ralendar_logo.png

--- web/src/components/events/GlobalSidebar.vue
+++ web/src/components/events/GlobalSidebar.vue
@@ -11,7 +11,7 @@
         <div class="sidebar-header">
           <h5>
             <img 
-              src="/static/images/ralendar_logo_final.png" 
+              src="/static/images/Ralendar_logo.png" 
               alt="Ralendar"
               class="sidebar-logo"
             >
